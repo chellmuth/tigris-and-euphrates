d652b720c12d85cd061a74844d1072029fa9df0b
Chris Hellmuth <cjh@manatee.(none)>
2008-06-30 23:20:03 -0700

    Unit test formatting

--- game/tests.py
+++ game/tests.py
@@ -12,18 +12,21 @@ class StandardBoardTestCase(unittest.TestCase):
 
         self.assertEquals([ (x.piece and str(x.piece.__class__),
                              x.special and str(x.special.__class__),
-                             str(x.__class__),) for x in board.cells ], 
-                          [(None, None, 'game.board.cell.Ground'), 
-                           (None, None, 'game.board.cell.River'),
-                           ('game.board.piece.SettlementCiv', None, 'game.board.cell.Ground'),
-                           ('game.board.piece.TempleCiv', None, 'game.board.cell.Ground'),
-                           ('game.board.piece.FarmCiv', None, 'game.board.cell.River'),
-                           ('game.board.piece.MerchantCiv', None, 'game.board.cell.Ground'),
-                           (None, 'game.board.special.Catastrophe', 'game.board.cell.Ground'),
-                           (None, 'game.board.special.Catastrophe', 'game.board.cell.River'),
-                           ('game.board.piece.MerchantCiv', 'game.board.special.Unification', 'game.board.cell.Ground'),
-                           ('game.board.piece.FarmCiv', 'game.board.special.Unification', 'game.board.cell.River'),
-                           ('game.board.piece.SettlementRuler', None, 'game.board.cell.Ground'),
-                           ('game.board.piece.TempleCiv', None, 'game.board.cell.Ground'),])
+                             str(x.__class__),) 
+                            for x in board.cells], 
+
+                          [(None,                               None,                             'game.board.cell.Ground'), 
+                           (None,                               None,                             'game.board.cell.River' ),
+                           ('game.board.piece.SettlementCiv',   None,                             'game.board.cell.Ground'),
+                           ('game.board.piece.TempleCiv',       None,                             'game.board.cell.Ground'),
+                           ('game.board.piece.FarmCiv',         None,                             'game.board.cell.River' ),
+                           ('game.board.piece.MerchantCiv',     None,                             'game.board.cell.Ground'),
+                           (None,                               'game.board.special.Catastrophe', 'game.board.cell.Ground'),
+                           (None,                               'game.board.special.Catastrophe', 'game.board.cell.River' ),
+                           ('game.board.piece.MerchantCiv',     'game.board.special.Unification', 'game.board.cell.Ground'),
+                           ('game.board.piece.FarmCiv',         'game.board.special.Unification', 'game.board.cell.River' ),
+                           ('game.board.piece.SettlementRuler', None,                             'game.board.cell.Ground'),
+                           ('game.board.piece.TempleCiv',       None,                             'game.board.cell.Ground'),
+                           ])
 
         self.assertEquals(board._db_form(), 'G|R|s|t|f|m|G!|R!|G?m|R?|r1s|T')
